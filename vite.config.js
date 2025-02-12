14da17e9bf005b315d2d5398c67cb0656dbaf7a3
Vladislav Chesnov <tazaqsp@mail.ru>
2025-02-12 18:25:55 +0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development'
+    ? '/'
+    : '/chat-gpt-driven-developed-sprint-points-calculator-9000/',
   plugins: [vue()],
 })
